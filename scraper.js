2510030cddef8de88334089e6aca8e07cf2239bc
れのらり <56919233+renorari@users.noreply.github.com>
2023-03-15 14:32:13 +0900

    更新

--- scraper.js
+++ scraper.js
@@ -1,31 +1,33 @@
-const { Client, Intents } = require("discord.js");
-const { readFileSync, writeFileSync } = require("fs");
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
-});
-
-var database = [];
-database = require("./scraped.json");
+const { Client, GatewayIntentBits } = require("discord.js");
+const { readFile, writeFile } = require("node:fs/promises");
+require("dotenv").config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity({"name": "学習中... | Training..."});
+    console.log(`${client.user.tag}としてログインしました`);
+    client.user.setActivity("取得中...", { type: "PLAYING" });
 });
 
 client.on("messageCreate", async (message) => {
     if (message.author.id != "698395012219666432") return;
-    let data = {
-        "message": message.cleanContent,
-        "channel": message.channelId,
-        "guild": message.guildId,
-        "time": message.createdAt,
-        "IsReference": message.reference ? true : false,
-        "ReferenceContent": message.reference ? (await message.fetchReference()).cleanContent : null,
-        "ReferenceAuther": message.reference ? (await message.fetchReference()).author.id : null
-    };
-    database.push(data);
-    writeFileSync("scraped.json", JSON.stringify(database), "utf-8");
-    console.log(data);
+    if (!message.content && message.content == "") return;
+    if (message.reference?.messageId) {
+        const reference = await message.fetchReference();
+        const messages = JSON.parse(await readFile("./messages.json", "utf-8"));
+        messages.push({
+            "reference": reference.content,
+            "message": message.content
+        });
+        await writeFile("./messages.json", JSON.stringify(messages), "utf-8");
+    } else {
+        const secondToLastMessage = (await message.channel.messages.fetch()).first(2)[1];
+        const messages = JSON.parse(await readFile("./messages.json", "utf-8"));
+        messages.push({
+            "reference": secondToLastMessage.content,
+            "message": message.content
+        });
+        await writeFile("./messages.json", JSON.stringify(messages), "utf-8");
+    }
 });
 
-client.login(readFileSync("DiscordBotToken.txt","utf-8"));
+client.login(process.env.DISCORD_TOKEN);
--- trainer.js
+++ trainer.js
@@ -1,49 +1,15 @@
+const { writeFileSync } = require("node:fs");
 const brain = require("brain.js");
-const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const messages = require("./messages.json");
+const ai = new brain.recurrent.GRU({
+    "hiddenLayers": [100,50]
 });
-const ScrapedData = require("./scraped.json");
-const trainingData = [];
-
-ScrapedData.forEach((data) => {
-    trainingData.push({
-        "input": [
-            data.channel,
-            data.guild,
-            data.time,
-            data.ReferenceContent ? data.ReferenceContent : "",
-            data.ReferenceAuther ? data.ReferenceAuther : ""
-        ],
-        "output": data.message
-    });
+const trainingData = messages.map(message => ({
+    "input": message.reference,
+    "output": message.message
+}));
+ai.train(trainingData, {
+    log: details => console.log(details),
+    errorThresh: 0.012
 });
-
-const ai = new brain.recurrent.GRU({ hiddenLayers: [300, 300] });
-const result = ai.train(trainingData, {
-    log: (details) => {
-        console.log(details);
-        console.log(ai.run([
-            "845316698961608774",
-            "726260425200762991",
-            new Date(),
-            "こんにちは",
-            "698395012219666432"
-        ]));
-    },
-    errorThresh: 0.011,
-});
-
-fs.writeFileSync("ai.json", JSON.stringify(ai.toJSON()), "utf-8");
-(function question() {
-    readline.question("> ", (answer) => {
-        console.log(ai.run([
-            "845316698961608774",
-            "726260425200762991",
-            new Date(),
-            answer,
-            "698395012219666432"
-        ]));
-        question();
-    });
-})();
+writeFileSync("./ai.json", JSON.stringify(ai.toJSON()), "utf-8");
